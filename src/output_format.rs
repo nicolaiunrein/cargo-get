571c4091bab1a0b6a01d579e41ebde53c976e7b3
Nicolai Unrein <n.unrein@gmail.com>
2025-06-06 20:22:34 +0200

    initial commit

--- src/output_format.rs
+++ src/output_format.rs
@@ -5,11 +5,26 @@ use crate::cli::Command;
 #[derive(Debug, PartialEq, Clone, ValueEnum)]
 pub enum OutputFormat {
     GithubAction,
+    Pretty,
 }
 
 impl OutputFormat {
     pub(crate) fn format_pair(&self, cmd: Command, value: &str) -> String {
-        const DELIMITER: &str = "EOF";
-        format!("{cmd}<<{DELIMITER}\n{value}\n{DELIMITER}\n")
+        match self {
+            Self::GithubAction => {
+                const DELIMITER: &str = "EOF";
+                format!("{cmd}<<{DELIMITER}\n{value}\n{DELIMITER}\n")
+            }
+            Self::Pretty => {
+                const INDENT: &str = "  ";
+                let style = anstyle::Style::new().fg_color(Some(anstyle::AnsiColor::Green.into()));
+                let lines = value
+                    .lines()
+                    .map(|line| format!("{INDENT}|>{INDENT}{line}\n"))
+                    .collect::<String>();
+
+                format!("\n{INDENT}{style}{cmd}{style:#}\n{lines}")
+            }
+        }
     }
 }
